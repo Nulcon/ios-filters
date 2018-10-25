cf0fc67292ac7022e312dbd9aeb56f58d793cdef
Conner Alegre <cga0@pm.me>
2018-10-25 13:42:16 -0600

    Initial commit

---
+++ ios-filters-mac-app/ios-filters-mac-app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ios-filters-mac-app
+//
+//  Created by Conner on 10/25/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ios-filters-mac-app/ios-filters-mac-app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-filters-mac-app
+//
+//  Created by Conner on 10/25/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
